89e5e06dde4994170b895c295ffd1b8344b183d2
Tan Wei Been <wbing2007@hotmail.my>
2022-11-25 18:05:58 +0800

    Commit for submission

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # MoneyLionTechAssessment - AI Engineer by WB (24/11/2022)
 
 Java 19 and Build with Gradle
+Hosted on AWS Lambda, Endpoint as follows: (https://73dln5ba1g.execute-api.ap-southeast-1.amazonaws.com/dev/feature)
+*If try the endpoints, please wait awhile on first call, as lambda need to do through cold start.
+ - GET /feature -> https://73dln5ba1g.execute-api.ap-southeast-1.amazonaws.com/dev/feature?email=XXX&featureName=YYY
+ - POST /feature -> https://73dln5ba1g.execute-api.ap-southeast-1.amazonaws.com/dev/feature
 
 ## To run the application
 
@@ -8,10 +12,30 @@ Java 19 and Build with Gradle
 ./gradlew bootRun
 ```
 
+## To build the application
+
+```
+./gradlew build
+```
+
+## To run test
+
+```
+./gradlew test
+```
+
+## Host on AWS Lambda via serverless package (change compatible in build.gradle to Java11)
+
+```
+npm install -g serverless
+serverless deploy --region ap-southeast-1
+```
+
 ## Task
 
 - [x] Requirement GET/POST API Endpoints
+- [x] Connected to managed PostgreSQL on cloud (ElephantSQL because is free!) 
 - [] Automated test
 - [] Frontend
-- [] Hosting & Domain
-- [] Submission
+- [x] Hosting
+- [] Domain Name
--- src/test/java/com/example/MoneyLionTechAssessment/MoneyLionTechAssessmentApplicationTests.java
+++ src/test/java/com/example/MoneyLionTechAssessment/MoneyLionTechAssessmentApplicationTests.java
@@ -1,13 +1,95 @@
 package com.example.MoneyLionTechAssessment;
 
-import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import com.example.MoneyLionTechAssessment.models.FeatureEmail;
+import com.example.MoneyLionTechAssessment.repositories.FeatureEmailRepository;
+import com.example.MoneyLionTechAssessment.services.FeatureEmailService;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class MoneyLionTechAssessmentApplicationTests {
+@AutoConfigureMockMvc
+class MoneyLionTechAssessmentApplicationAPIIntegrationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private FeatureEmailRepository featureEmailRepository;
+
+	@MockBean
+	private FeatureEmailService featureEmailService;
 
 	@Test
-	void contextLoads() {
+	/*
+	 * Current assumption: featureEmailService.toggleFeaturebyEmail is working well and unit tested.
+	 */
+	public void testGetFeature() throws Exception {
+
+		// no query args, expect canAcess false
+		this.mockMvc.perform(get("/feature"))
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.canAccess").value(false));
+
+		// query some random values, expect to false
+		this.mockMvc.perform(get("/feature").param("featureName", "randomFeature12321"))
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.canAccess").value(false));
+
+		// toggle enable to false with corresponding featureName/Email, expect false
+		String testFeature1 = "testFeature1";
+		String testEmail1 = "testEmail1";
+		Boolean testToggle1 = false;
+		featureEmailService.toggleFeaturebyEmail(testFeature1, testEmail1, testToggle1);
+		this.mockMvc.perform(get("/feature").param("featureName", testFeature1).param("email", testEmail1))
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.canAccess").value(testToggle1));
+
+		// toggle enable to true with corresponding featureName/Email, expect true
+		// testFeature1 = "testFeature1";
+		// testEmail1 = "testEmail1";
+		// testToggle1 = true;
+		// Boolean success_db_update = featureEmailService.toggleFeaturebyEmail(testFeature1, testEmail1, testToggle1);
+		// System.out.println(success_db_update);
+		// this.mockMvc.perform(get("/feature").param("featureName", testFeature1).param("email", testEmail1))
+		// .andExpect(status().isOk())
+		// .andExpect(MockMvcResultMatchers.jsonPath("$.canAccess").value(testToggle1));
+
+	}
+
+	@Test
+	public void testPostFeature() throws Exception {
+
+		// first test: change to True, expect Ok
+		// String jsonString = "{\"featureName\": \"fea1\", \"email\": \"email11\", \"enable\": \"true\"}";
+		// this.mockMvc.perform(post("/feature").content(jsonString)
+		// .contentType(MediaType.APPLICATION_JSON)
+		// .accept(MediaType.APPLICATION_JSON))
+		// .andExpect(status().isOk());
+
+		// Optional<FeatureEmail> foundFeatureEmail = featureEmailRepository.findByFeature_FeatureNameAndEmail_Email("fea1", "email11");
+		// assertThat(foundFeatureEmail.get().isEnable()).isEqualTo(true);
+
+		// // second test: submit similar request, expect Not Modified
+		// this.mockMvc.perform(post("/feature").content(jsonString)
+		// .contentType(MediaType.APPLICATION_JSON)
+		// .accept(MediaType.APPLICATION_JSON))
+		// .andExpect(status().isNotModified());
+
+		// more edge cases ..
+
 	}
 
 }
